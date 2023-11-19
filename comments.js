6608782c531d4de88a2157aa9d5dc0f7c043a77c
htrad1 <150966807+htrad1@users.noreply.github.com>
2023-11-19 13:15:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,19 @@
+// Create web server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+const comments = require('./routes/api/comments');
+
+app.use('/api/comments', comments);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
